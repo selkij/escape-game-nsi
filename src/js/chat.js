8292b605dbe4a9bacad408f1aa2b0343ed002f24
Thomas <mathwo37@outlook.fr>
2025-02-10 12:27:53 +0100

    supprimage de TODO inutiles

--- src/js/chat.js
+++ src/js/chat.js
@@ -1,4 +1,4 @@
-// TODO: Fonction qui ajoute une balise <p> dans la class message-box, de texte "user à dit:<br> message" 
+
 function send_message(user, message) {
     let div = document.getElementById('message-box');
     let element_p = document.createElement('p');
